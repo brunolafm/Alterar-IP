1a45708d7d1635355e3115c66c0469fc2ffbf77e
Bruno Lisboa Furquim <brunolafm@gmail.com>
2016-05-04 17:43:10 -0300

    Espaços removidos
    Foram removidos alguns espaços desnecessários

--- ip.bat
+++ ip.bat
@@ -25,7 +25,7 @@ if %choose%==3 goto sair
 REM DHCP
 :dhcp
 cls
-runas /savecred /user:Administrator "netsh interface ip set address name= Wi-Fi dhcp"
+runas /savecred /user:Administrator "netsh interface ip set address name=Wi-Fi dhcp"
 runas /savecred /user:Administrator "netsh interface ip set dnsservers name=Wi-Fi dhcp"
 goto sair
 
@@ -45,7 +45,7 @@ if %choose%==2 goto prefeitura
 REM Configuracoes de Casa
 :casa
 cls
-runas /savecred /user:Administrator "netsh interface ip set address name= Wi-Fi static 192.168.0.200 255.255.255.0 192.168.0.1"
+runas /savecred /user:Administrator "netsh interface ip set address name=Wi-Fi static 192.168.0.200 255.255.255.0 192.168.0.1"
 runas /savecred /user:Administrator "netsh interface ip add dns name=Wi-Fi 200.195.159.100 index=1"
 runas /savecred /user:Administrator "netsh interface ip add dns name=Wi-Fi 189.38.95.95 index=2"
 goto sair
@@ -53,7 +53,7 @@ goto sair
 REM Configuracoes da Prefeitura
 :prefeitura
 cls
-runas /savecred /user:Administrator "netsh interface ip set address name= Wi-Fi static 10.0.0.101 255.255.255.0 10.0.0.1"
+runas /savecred /user:Administrator "netsh interface ip set address name=Wi-Fi static 10.0.0.101 255.255.255.0 10.0.0.1"
 runas /savecred /user:Administrator "netsh interface ip add dns name=Wi-Fi 200.195.159.100 index=1"
 runas /savecred /user:Administrator "netsh interface ip add dns name=Wi-Fi 189.38.95.95 index=2"
 goto sair
